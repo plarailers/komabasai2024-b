77fc0f18c1432787d9a5e416d166a99009574252
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-10-14 01:18:31 +0900

    ラズパイ用のコードを追加

---
+++ raspi_main/README.md
@@ -0,0 +1,28 @@
+# raspi_main
+
+- main.py
+  - Raspberry Pi 上で動かすスクリプト。ビデオの送信、メッセージの送受信、GPIO の操作を行う。
+- index.html
+  - Raspberry Pi で配信し、PC のブラウザ上で動かす。ビデオの受信、メッセージの送受信を行う。
+
+## Raspberry Pi 側で必要なライブラリ
+
+```
+sudo apt install socat
+pip3 install pyserial
+```
+
+## PC から Raspberry Pi へのファイル転送
+
+```
+scp -r ./raspi_main pi@raspberrypi.local:~/raspi_main
+```
+
+## 実行
+
+```
+cd raspi_main
+python3 main.py
+```
+
+http://raspberrypi.local:8080/ にアクセスする。
---
+++ raspi_main/main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import subprocess
+import re
+import os.path
+import time
+from queue import Queue
+import serial
+
+MOMO_BIN = os.path.expanduser('~/momo-2020.8.1_raspberry-pi-os_armv6/momo')
+
+process_socat = None
+process_momo = None
+port = None
+
+recv_queue = Queue()
+send_queue = Queue()
+
+def setup():
+    global process_socat, process_momo, port
+    print('starting...')
+    process_socat = subprocess.Popen(['socat', '-d', '-d', 'pty,raw,echo=0', 'pty,raw,echo=0'], stderr=subprocess.PIPE)
+    port1_name = re.search('N PTY is (\\S+)', process_socat.stderr.readline().decode()).group(1)
+    port2_name = re.search('N PTY is (\\S+)', process_socat.stderr.readline().decode()).group(1)
+    process_socat.stderr.readline()
+    print('using ports', port1_name, 'and', port2_name)
+    process_momo = subprocess.Popen([MOMO_BIN, '--no-audio-device', '--use-native', '--force-i420', '--serial', port1_name + ',9600', 'test'])
+    port = serial.Serial(port2_name, 9600)
+    print('started')
+    print('running at http://raspberrypi.local:8080/')
+    print('ctrl+c to quit')
+
+def loop():
+    while port.in_waiting > 0:
+        data = port.read()
+        print(data)
+
+    while not send_queue.empty():
+        data = send_queue.get()
+        port.write(data)
+        port.flush()
+
+if __name__ == '__main__':
+    try:
+        setup()
+        while True:
+            loop()
+            time.sleep(0.01)
+    except KeyboardInterrupt:
+        print("interrupted")
+    except Exception as e:
+        print(e)
+    finally:
+        if port:
+            port.close()
+        if process_momo:
+            process_momo.terminate()
+        if process_socat:
+            process_socat.terminate()
