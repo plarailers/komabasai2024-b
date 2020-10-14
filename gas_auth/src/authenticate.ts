e8f54a5571da151eb170f2d65c366244449a7399
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-10-14 17:08:32 +0900

    認証情報を返す

--- gas_auth/src/authenticate.ts
+++ gas_auth/src/authenticate.ts
@@ -1,8 +1,26 @@
-function doGet(): GoogleAppsScript.Content.TextOutput {
+function doGet(e: GoogleAppsScript.Events.AppsScriptHttpRequestEvent): GoogleAppsScript.Content.TextOutput {
+  const peerId = e.parameter['peerId'];
+  const timestamp = Math.floor(Date.now() / 1000);
+  const ttl = 300;
+  const credential = {
+    peerId,
+    timestamp,
+    ttl,
+    authToken: calculateAuthToken(peerId, timestamp, ttl),
+  };
   const output = ContentService.createTextOutput();
   output.setMimeType(ContentService.MimeType.JSON);
-  output.setContent(JSON.stringify({
-    text: 'hello'
-  }));
+  const content = JSON.stringify({
+    code: 0,
+    credential,
+  });
+  Logger.log(content);
+  output.setContent(content);
   return output;
+}
+
+function calculateAuthToken(peerId: string, timestamp: number, ttl: number): string {
+  const secretKey = PropertiesService.getScriptProperties().getProperty('SKYWAY_SECRET_KEY');
+  const hash = Utilities.computeHmacSha256Signature(`${timestamp}:${ttl}:${peerId}`, secretKey);
+  return Utilities.base64Encode(hash);
 }
