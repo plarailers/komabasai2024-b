0ee9f35c04e76919f866aae8d5909fa7b0aa643f
n4o847 <22975590+n4o847@users.noreply.github.com>
2023-04-22 23:49:09 +0900

    ptcs_client を再生成

--- ptcs/ptcs_client/dist/index.ts
+++ ptcs/ptcs_client/dist/index.ts
@@ -7,13 +7,16 @@ export { OpenAPI } from './core/OpenAPI';
 export type { OpenAPIConfig } from './core/OpenAPI';
 
 export { Direction } from './models/Direction';
+export type { HTTPValidationError } from './models/HTTPValidationError';
 export type { JunctionConfig } from './models/JunctionConfig';
 export type { JunctionState } from './models/JunctionState';
+export type { MoveTrainParams } from './models/MoveTrainParams';
 export type { RailwayConfig } from './models/RailwayConfig';
 export type { RailwayState } from './models/RailwayState';
 export type { SectionConfig } from './models/SectionConfig';
 export type { SectionState } from './models/SectionState';
 export type { TrainConfig } from './models/TrainConfig';
 export type { TrainState } from './models/TrainState';
+export type { ValidationError } from './models/ValidationError';
 
 export { DefaultService } from './services/DefaultService';
--- ptcs/ptcs_client/dist/services/DefaultService.ts
+++ ptcs/ptcs_client/dist/services/DefaultService.ts
@@ -1,6 +1,7 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
+import type { MoveTrainParams } from '../models/MoveTrainParams';
 import type { RailwayConfig } from '../models/RailwayConfig';
 import type { RailwayState } from '../models/RailwayState';
 
@@ -43,6 +44,33 @@ export class DefaultService {
         return __request(OpenAPI, {
             method: 'GET',
             url: '/api/state',
+        });
+    }
+
+    /**
+     * Move Train
+     * 指定された列車を距離 delta 分だけ進める。
+     * デバッグ用。
+     * @param trainId
+     * @param requestBody
+     * @returns any Successful Response
+     * @throws ApiError
+     */
+    public static moveTrain(
+        trainId: string,
+        requestBody: MoveTrainParams,
+    ): CancelablePromise<any> {
+        return __request(OpenAPI, {
+            method: 'POST',
+            url: '/api/state/trains/{train_id}/move',
+            path: {
+                'train_id': trainId,
+            },
+            body: requestBody,
+            mediaType: 'application/json',
+            errors: {
+                422: `Validation Error`,
+            },
         });
     }
 
