2a8f1c6382b7d4f4b0fb9cad393ec791e110c727
n4o847 <22975590+n4o847@users.noreply.github.com>
2023-04-22 17:35:07 +0900

    列車を移動させる API を追加

--- ptcs/ptcs_server/api.py
+++ ptcs/ptcs_server/api.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, Request
 from ptcs_control import Control
+from ptcs_control.components import Train
 from ptcs_control.railway_config import RailwayConfig
 from ptcs_control.railway_state import RailwayState
+import pydantic
 
 
 api_router = APIRouter()
@@ -22,3 +24,18 @@ def get_config(request: Request) -> "RailwayConfig":
 def get_state(request: Request) -> "RailwayState":
     control: Control = request.app.state.control
     return control.get_state()
+
+
+class MoveTrainParams(pydantic.BaseModel):
+    delta: float
+
+
+@api_router.post("/state/trains/{train_id}/move")
+def move_train(train_id: str, params: MoveTrainParams, request: Request) -> None:
+    """
+    指定された列車を距離 delta 分だけ進める。
+    デバッグ用。
+    """
+    control: Control = request.app.state.control
+    train = Train(train_id)
+    control.move_train(train, params.delta)
