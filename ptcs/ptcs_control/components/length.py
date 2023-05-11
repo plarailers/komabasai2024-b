c7815e59913d464c258f2d5b893af43bb4d97fff
Ryota-Nitto <nitryo.run0524@gmail.com>
2023-05-11 23:13:23 +0900

    railway_configを更新

--- ptcs/ptcs_control/components/length.py
+++ ptcs/ptcs_control/components/length.py
@@ -1,9 +1,9 @@
 # レール長さ
 # https://scrapbox.io/plarailers-private/レール長さ一覧
-STRAIGHT_RAIL = 215.0  # 直線レール [mm]
-STRAIGHT_1_6_RAIL = 36.5  # 1/6直線レール [mm]
-CURVE_RAIL = 169.0  # 曲線レール [mm]
-U_TURN_RAIL = 1010.0  # Uターンレール [mm]
-WATARI_RAIL_A = 185.0  # 複線わたりポイントレール直線部(長) [mm]
-WATARI_RAIL_B = 30.0  # 複線わたりポイントレール直線部(短) [mm]
-WATARI_RAIL_C = 165.0  # 複線わたりポイントレールわたり部 [mm]
+STRAIGHT_RAIL = 21.50  # 直線レール [cm]
+STRAIGHT_1_6_RAIL = 3.65  # 1/6直線レール [cm]
+CURVE_RAIL = 16.90  # 曲線レール [cm]
+U_TURN_RAIL = 101.00  # Uターンレール [cm]
+WATARI_RAIL_A = 18.50  # 複線わたりポイントレール直線部(長) [cm]
+WATARI_RAIL_B = 3.00  # 複線わたりポイントレール直線部(短) [cm]
+WATARI_RAIL_C = 16.50  # 複線わたりポイントレールわたり部 [cm]
