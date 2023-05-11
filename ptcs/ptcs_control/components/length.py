86cba0d57822f7a12cb193b4c460cfc5b4a2723b
thgcMtdh <kyouho315@gmail.com>
2023-05-11 21:39:29 +0900

    レール長さを追加

--- ptcs/ptcs_control/components/length.py
+++ ptcs/ptcs_control/components/length.py
@@ -1,2 +1,9 @@
-STRAIGHT_UNIT = 215.0  # 直線レールの長さ [mm]
-CURVE_UNIT = 169.0  # 曲線レールの長さ [mm]
+# レール長さ
+# https://scrapbox.io/plarailers-private/レール長さ一覧
+STRAIGHT_RAIL = 215.0  # 直線レール [mm]
+STRAIGHT_1_6_RAIL = 36.5  # 1/6直線レール [mm]
+CURVE_RAIL = 169.0  # 曲線レール [mm]
+U_TURN_RAIL = 1010.0  # Uターンレール [mm]
+WATARI_RAIL_A = 185.0  # 複線わたりポイントレール直線部(長) [mm]
+WATARI_RAIL_B = 30.0  # 複線わたりポイントレール直線部(短) [mm]
+WATARI_RAIL_C = 165.0  # 複線わたりポイントレールわたり部 [mm]
