0061bea5ff4ae8f397867cdd2d5d77b25d1492da
thgcMtdh <kyouho315@gmail.com>
2023-05-12 19:42:26 +0900

    基地局はTrainに接続できるまでconnectを繰り返す

--- pc_changable/Usb_bt_bridge/Usb_bt_bridge.ino
+++ pc_changable/Usb_bt_bridge/Usb_bt_bridge.ino
@@ -3,26 +3,26 @@
 
 BluetoothSerial SerialBT;
 
-bool connected;
+bool connected = false;
 const int BUFFER_SIZE = 32;
 char buf[BUFFER_SIZE];
 char buf2[BUFFER_SIZE];
 
 void setup() {
+    Serial.begin(115200);
+    SerialBT.begin("ESP32_sender", true); 
+    //上のtrueはmasterであることを示す
 
-  pinMode( 5, OUTPUT);
-  Serial.begin(115200);
-  SerialBT.begin("ESP32_sender", true); 
-  //上のtrueはmasterであることを示す
-  connected = SerialBT.connect("ESP32-Dr");
-  //addressはname or Macaddressを引数にとる。前者はmax30ms後者はmax10ms程度の秒数で接続可能
-  
-    if(connected){
-        Serial.println("Connected Succesfully!");
-    }
-    else {
-        while(!SerialBT.connected(10000)) {
-        Serial.println("Failed to connect. Make sure remote device is available and in range, then restart app."); 
+    while (!connected) {
+        Serial.println("Connecting...");
+        connected = SerialBT.connect("ESP32-E5");
+        //addressはname or Macaddressを引数にとる。前者はmax30ms後者はmax10ms程度の秒数で接続可能
+
+        if(connected){
+            Serial.println("Connected Succesfully!");
+        }
+        else {
+            Serial.println("Failed to connect. Make sure remote device is available and in range, then restart app."); 
         }
     }
   
