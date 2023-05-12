dcf18e55476cbe02c364651d3114084432da6c61
n4o847 <22975590+n4o847@users.noreply.github.com>
2023-05-12 16:29:28 +0900

    横の navbar を削除 (邪魔なので)

--- ptcs/ptcs_ui/src/components/Layout.tsx
+++ ptcs/ptcs_ui/src/components/Layout.tsx
@@ -1,41 +1,9 @@
-import { NavLink as ReactRouterNavLink } from "react-router-dom";
-import { AppShell, Header, Navbar, NavLink, Stack } from "@mantine/core";
+import { AppShell, Header } from "@mantine/core";
 
 export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <AppShell
       padding="md"
-      navbar={
-        <Navbar width={{ base: 300 }}>
-          <Stack p="xs">
-            {[
-              { label: "Home", to: "/" },
-              { label: "Settings", to: "/settings" },
-            ].map((item) => (
-              <ReactRouterNavLink
-                key={item.label}
-                to={item.to}
-                style={{ textDecoration: "none" }}
-              >
-                {({ isActive }) => (
-                  <NavLink
-                    label={item.label}
-                    active={isActive}
-                    sx={(theme) => ({
-                      display: "flex",
-                      alignItems: "center",
-                      color: theme.colors.gray[7],
-                      padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
-                      borderRadius: theme.radius.md,
-                      fontWeight: 500,
-                    })}
-                  />
-                )}
-              </ReactRouterNavLink>
-            ))}
-          </Stack>
-        </Navbar>
-      }
       header={
         <Header
           height={60}
