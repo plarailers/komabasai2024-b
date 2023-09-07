a6da65f4559f03a3426341ab5779fb74dadee56a
Ryota-Nitto <nitryo.run0524@gmail.com>
2023-09-07 17:22:06 +0900

    暫定版(動かない

---
+++ rotary_encoder/rotary_encoder.ino
@@ -0,0 +1,22 @@
+const int pin = 25;
+int counter = 0, preCounter = 0;
+volatile unsigned long preTime = 0, nowTime;
+unsigned long chattaringTime = 10;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(pin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(pin), count, CHANGE);
+}
+
+void loop() {
+  Serial.println(counter);
+}
+
+void count() {
+  nowTime = millis();
+  if(nowTime - preTime > chattaringTime){
+    counter += 1;
+    preTime = nowTime;
+  }
+}
