fbe5b27fa5e39f4284a562fd00b3143f5b77b96a
Taro-Suzuki-TwilightEXP <suzuki.taro.twilightexp@gmail.com>
2023-10-13 18:21:26 +0900

    BLE通信部分が完成しました

--- master_controller/master_controller.ino
+++ master_controller/master_controller.ino
@@ -4,9 +4,85 @@
 #define SEGMENT_ON HIGH
 #define SEGMENT_OFF LOW
 
-#include <BluetoothSerial.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#include <driver/adc.h>
 
-BluetoothSerial SerialBT;//BluetoothSerialオブジェクトを作成
+
+static const char SERVICE_UUID[] = "cea8c671-fb2c-5f3c-87ea-7ddea950b9a5";
+static const char CHARACTERISTIC_VELOCITYINSTRUCTION_UUID[] = "4bb36d3a-dace-c0e6-e70c-81e0e77930cb";
+
+BLEServer *pServer = NULL;
+BLEService *pService = NULL;
+BLECharacteristic *pCharacteristicVelocityInstruction = NULL;
+BLEAdvertising *pAdvertising = NULL;
+
+/*---------------- BLE 関数 ここから --------------------*/
+
+class MyServerCallbacks : public BLEServerCallbacks {
+  /// @brief BLEサーバーとの接続．切れたときにモータ停止
+  /// @param pServer 
+  void onConnect(BLEServer* pServer) {
+    Serial.println("BLE Connected");
+  }
+
+  void onDisconnect(BLEServer *pServer) {
+    Serial.println("BLE Disconnected");
+    pServer->startAdvertising();
+  }
+};
+
+void bleSetup() {
+  /// @brief BLEのセットアップ．
+  /// 1. サーバー，サービスを設定モータ入力，
+  /// 2. 位置ID，ロータリエンコーダの3つのキャラクタリスティックを作る．
+  /// 3. BLE通信を開始
+
+  Serial.println("Starting BLE");
+
+  Serial.print("Chip ID: ");
+  Serial.println(ESP.getEfuseMac());
+
+  BLEDevice::init("master controller");
+
+  Serial.print("Address: ");
+  Serial.println(BLEDevice::getAddress().toString().c_str());
+
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  pService = pServer->createService(SERVICE_UUID);
+
+  pCharacteristicVelocityInstruction = pService->createCharacteristic(
+      CHARACTERISTIC_VELOCITYINSTRUCTION_UUID,
+      BLECharacteristic::PROPERTY_NOTIFY
+  );
+  pCharacteristicVelocityInstruction->setValue("Initial value");
+  pCharacteristicVelocityInstruction->addDescriptor(new BLE2902());
+
+  pService->start();
+
+  pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->start();
+  Serial.println("Advertising started");
+}
+
+/*----------- BLE 関数 ここまで --------------------------*/
+
+int velocity = 0;
+int acceleration = 0;
+int lasttime = 0;
+int velocitydisplay = 0;
+
+void sendVelocity(){
+
+  pCharacteristicVelocityInstruction->setValue((uint8_t*)&velocity, 4);
+  pCharacteristicVelocityInstruction->notify();
+  Serial.printf("velocity: %d Notified\n", velocity);
+}
 
 const int digitPins[4] = {13,14,15,16}; //コモン(ディジット)のピン番号を配列で指定
 const int segmentPins[8] = {17,18,19,21,22,23,25,26}; //セグメントのピン番号を配列で指定
@@ -70,12 +146,12 @@ void displayNumber(int n){
   }
 }
 
-int velocity = 0;
-int acceleration = 0;
-int lasttime = 0;
-int velocitydisplay = 0;
 
 void setup() {
+
+  /* BLE セットアップ */
+  bleSetup();
+
   for (int i = 0; i <= 3; i = i + 1){
     pinMode(digitPins[i],OUTPUT);
     digitalWrite(digitPins[i],DIGIT_OFF);
@@ -88,9 +164,6 @@ void setup() {
 
   Serial.begin(115200);
   pinMode(27,INPUT);
-
-  SerialBT.begin(device_name);
-  Serial.printf("The device with name \"%s\" is started.\n Now you can pair it with Bluetooth!\n", device_name.c_str());
 
 }
 
@@ -123,7 +196,8 @@ void loop() {
 
 
     Serial.println(velocity);
-    SerialBT.println(velocity);
+    sendVelocity();
+
     lasttime = millis();
   }
 
