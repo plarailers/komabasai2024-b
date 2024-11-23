c05325eb04656050fb8582dd2439e2113938d1a1
n4o847 <22975590+n4o847@users.noreply.github.com>
2024-11-23 11:58:03 +0900

    feat: いずれかのタスクでエラーが出たときに接続を正常終了させる

--- ptcs/ptcs_server/server.py
+++ ptcs/ptcs_server/server.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import os
+import traceback
 from contextlib import asynccontextmanager
 
 import uvicorn
@@ -58,7 +59,10 @@ def handle_task_error(task: asyncio.Task):
         pass
     except Exception as e:
         logger.error(f"Task failed with error: {e}")
-        raise e
+        # KeyboardInterrupt にするとスタックトレースが出ないためここで表示
+        traceback.print_exc()
+        # サーバーを終了させるため KeyboardInterrupt として返す
+        raise KeyboardInterrupt() from e
 
 
 @asynccontextmanager
@@ -80,158 +84,155 @@ async def lifespan(app: FastAPI):
     bridge = create_bridge()
     app.state.bridge = bridge
 
-    ready_queue: asyncio.Queue[None] = asyncio.Queue()  # 各タスクが ready になったら put
-    ready_event = asyncio.Event()  # 全タスクが ready になったら notify
+    tasks: list[asyncio.Task] = []
 
-    async def control_loop():
-        for _ in range(len(bridge.trains) + len(bridge.points)):
-            await ready_queue.get()
-        logger.info("All trains and points are ready")
-        ready_event.set()
+    try:
+        ready_queue: asyncio.Queue[None] = asyncio.Queue()  # 各タスクが ready になったら put
+        ready_event = asyncio.Event()  # 全タスクが ready になったら notify
 
-        while True:
-            # control 内部の時計を現実世界の時間において進める
-            await asyncio.sleep(0.1)
-            control.tick()
-            control.update()
+        async def control_loop():
+            for _ in range(len(bridge.trains) + len(bridge.points)):
+                await ready_queue.get()
+            logger.info("All trains and points are ready")
+            ready_event.set()
 
-    control_loop_task = asyncio.create_task(control_loop())
-    control_loop_task.add_done_callback(handle_task_error)
-    app.state.control_loop_task = control_loop_task
+            while True:
+                # control 内部の時計を現実世界の時間において進める
+                await asyncio.sleep(0.1)
+                control.tick()
+                control.update()
 
-    async def train_loop(train_client: TrainBase):
-        await train_client.connect()
+        control_loop_task = asyncio.create_task(control_loop())
+        control_loop_task.add_done_callback(handle_task_error)
+        tasks.append(control_loop_task)
 
-        def handle_notify_position_uid(train_client: TrainBase, position_uid: str):
-            train_control = control.trains.get(train_client.id)
-            position = next(filter(lambda sp: sp.uid == position_uid, control.sensor_positions.values()), None)
-            if train_control is None or position is None:
-                return
-            train_control.fix_position(position)
+        async def train_loop(train_client: TrainBase):
+            await train_client.connect()
 
-        def handle_notify_rotation(train_client: TrainBase, _rotation: int):
-            train_control = control.trains.get(train_client.id)
-            if train_control is None:
-                return
-            train_control.move_forward_mr(1)
+            def handle_notify_position_uid(train_client: TrainBase, position_uid: str):
+                train_control = control.trains.get(train_client.id)
+                position = next(filter(lambda sp: sp.uid == position_uid, control.sensor_positions.values()), None)
+                if train_control is None or position is None:
+                    return
+                train_control.fix_position(position)
 
-        def handle_notify_voltage(train_client: TrainBase, _voltage_mV: int):
-            train_control = control.trains.get(train_client.id)
-            if train_control is None:
-                return
-            train_control.voltage_mV = _voltage_mV
+            def handle_notify_rotation(train_client: TrainBase, _rotation: int):
+                train_control = control.trains.get(train_client.id)
+                if train_control is None:
+                    return
+                train_control.move_forward_mr(1)
 
-        await train_client.start_notify_rotation(handle_notify_rotation)
-        match train_client:
-            case TrainClient():
-                await train_client.start_notify_position_uid(handle_notify_position_uid)
-                await train_client.start_notify_voltage(handle_notify_voltage)
+            def handle_notify_voltage(train_client: TrainBase, _voltage_mV: int):
+                train_control = control.trains.get(train_client.id)
+                if train_control is None:
+                    return
+                train_control.voltage_mV = _voltage_mV
 
-        train_control = control.trains.get(train_client.id)
-        if train_control is None:
-            logger.warning(f"{train_client} has no corresponding train")
-            return
-
-        logger.info(f"{train_client} is ready")
-        await ready_queue.put(None)
-        await ready_event.wait()
-
-        while True:
-            await asyncio.sleep(0.2)
+            await train_client.start_notify_rotation(handle_notify_rotation)
             match train_client:
-                case TrainSimulator():
-                    await train_client.send_speed(train_control.speed_command)
                 case TrainClient():
-                    motor_input = train_control.calc_input(train_control.speed_command)
-                    await train_client.send_motor_input(motor_input)
+                    await train_client.start_notify_position_uid(handle_notify_position_uid)
+                    await train_client.start_notify_voltage(handle_notify_voltage)
 
-    app.state.train_loop_tasks = {}
-    for train_id, train_client in bridge.trains.items():
-        train_loop_task = asyncio.create_task(train_loop(train_client))
-        train_loop_task.add_done_callback(handle_task_error)
-        app.state.train_loop_tasks[train_id] = train_loop_task
-
-    async def point_loop(point_client: PointClient):
-        await point_client.connect()
-
-        junction_control = control.junctions.get(point_client.id)
-        if junction_control is None:
-            logger.warning(f"{point_client} has no corresponding junction")
-            return
-
-        logger.info(f"{point_client} is ready")
-        await ready_queue.put(None)
-        await ready_event.wait()
-
-        while True:
-            await asyncio.sleep(0.2)
-            await point_client.send_direction(junction_control.current_direction)
-
-    app.state.point_loop_tasks = {}
-    for point_id, point_client in bridge.points.items():
-        point_loop_task = asyncio.create_task(point_loop(point_client))
-        point_loop_task.add_done_callback(handle_task_error)
-        app.state.point_loop_tasks[point_id] = point_loop_task
-
-    async def obstacle_loop(obstacle_client: WirePoleClient):
-        await obstacle_client.connect()
-
-        def handle_notify_collapse(obstacle_client: WirePoleClient, is_collapsed: bool):
-            obstacle_control = control.obstacles.get(obstacle_client.id)
-            if obstacle_control is None:
-                logger.warning(f"{obstacle_client} has no corresponding obstacle")
-                return
-            obstacle_control.is_detected = is_collapsed
-
-        await obstacle_client.start_notify_collapse(handle_notify_collapse)
-
-    app.state.obstacle_loop_tasks = {}
-    for obstacle_id, obstacle_client in bridge.obstacles.items():
-        obstacle_loop_task = asyncio.create_task(obstacle_loop(obstacle_client))
-        obstacle_loop_task.add_done_callback(handle_task_error)
-        app.state.obstacle_loop_tasks[obstacle_id] = obstacle_loop_task
-
-    async def controller_loop(controller_client: MasterControllerClient):
-        await controller_client.connect()
-
-        def handle_notify_speed(controller_client: MasterControllerClient, speed: int):
-            train_control = control.trains.get(controller_client.id)
+            train_control = control.trains.get(train_client.id)
             if train_control is None:
-                logger.warning(f"{controller_client} has no corresponding train")
+                logger.warning(f"{train_client} has no corresponding train")
                 return
-            train_control.manual_speed = speed / 255 * train_control.max_speed
 
-        await controller_client.start_notify_speed(handle_notify_speed)
+            logger.info(f"{train_client} is ready")
+            await ready_queue.put(None)
+            await ready_event.wait()
 
-    app.state.controller_loop_tasks = {}
-    for controller_id, controller_client in bridge.controllers.items():
-        controller_loop_task = asyncio.create_task(controller_loop(controller_client))
-        controller_loop_task.add_done_callback(handle_task_error)
-        app.state.controller_loop_tasks[controller_id] = controller_loop_task
+            while True:
+                await asyncio.sleep(0.2)
+                match train_client:
+                    case TrainSimulator():
+                        await train_client.send_speed(train_control.speed_command)
+                    case TrainClient():
+                        motor_input = train_control.calc_input(train_control.speed_command)
+                        await train_client.send_motor_input(motor_input)
 
-    # ここでサーバーのループが走る
-    yield
+        for train_id, train_client in bridge.trains.items():
+            train_loop_task = asyncio.create_task(train_loop(train_client))
+            train_loop_task.add_done_callback(handle_task_error)
+            tasks.append(train_loop_task)
 
-    logger.info("Exiting...")
+        async def point_loop(point_client: PointClient):
+            await point_client.connect()
 
-    app.state.control_loop_task.cancel()
-    for task in (
-        *app.state.train_loop_tasks.values(),
-        *app.state.point_loop_tasks.values(),
-        *app.state.obstacle_loop_tasks.values(),
-        *app.state.controller_loop_tasks.values(),
-    ):
-        task.cancel()
+            junction_control = control.junctions.get(point_client.id)
+            if junction_control is None:
+                logger.warning(f"{point_client} has no corresponding junction")
+                return
 
-    for train in bridge.trains.values():
-        match train:
-            case TrainSimulator():
-                await train.send_speed(0.0)
-            case TrainClient():
-                if train.is_connected:
-                    await train.send_motor_input(0)
+            logger.info(f"{point_client} is ready")
+            await ready_queue.put(None)
+            await ready_event.wait()
 
-    await bridge.disconnect_all()
+            while True:
+                await asyncio.sleep(0.2)
+                await point_client.send_direction(junction_control.current_direction)
+
+        for point_id, point_client in bridge.points.items():
+            point_loop_task = asyncio.create_task(point_loop(point_client))
+            point_loop_task.add_done_callback(handle_task_error)
+            tasks.append(point_loop_task)
+
+        async def obstacle_loop(obstacle_client: WirePoleClient):
+            await obstacle_client.connect()
+
+            def handle_notify_collapse(obstacle_client: WirePoleClient, is_collapsed: bool):
+                obstacle_control = control.obstacles.get(obstacle_client.id)
+                if obstacle_control is None:
+                    logger.warning(f"{obstacle_client} has no corresponding obstacle")
+                    return
+                obstacle_control.is_detected = is_collapsed
+
+            await obstacle_client.start_notify_collapse(handle_notify_collapse)
+
+        for obstacle_id, obstacle_client in bridge.obstacles.items():
+            obstacle_loop_task = asyncio.create_task(obstacle_loop(obstacle_client))
+            obstacle_loop_task.add_done_callback(handle_task_error)
+            tasks.append(obstacle_loop_task)
+
+        async def controller_loop(controller_client: MasterControllerClient):
+            await controller_client.connect()
+
+            def handle_notify_speed(controller_client: MasterControllerClient, speed: int):
+                train_control = control.trains.get(controller_client.id)
+                if train_control is None:
+                    logger.warning(f"{controller_client} has no corresponding train")
+                    return
+                train_control.manual_speed = speed / 255 * train_control.max_speed
+
+            await controller_client.start_notify_speed(handle_notify_speed)
+
+        for controller_id, controller_client in bridge.controllers.items():
+            controller_loop_task = asyncio.create_task(controller_loop(controller_client))
+            controller_loop_task.add_done_callback(handle_task_error)
+            tasks.append(controller_loop_task)
+
+        # ここでサーバーのループが走る
+        yield
+
+    except asyncio.exceptions.CancelledError:
+        pass
+
+    finally:
+        logger.info("Exiting...")
+
+        for task in tasks:
+            task.cancel()
+
+        for train in bridge.trains.values():
+            match train:
+                case TrainSimulator():
+                    await train.send_speed(0.0)
+                case TrainClient():
+                    if train.is_connected:
+                        await train.send_motor_input(0)
+
+        await bridge.disconnect_all()
 
 
 def serve(*, port: int = DEFAULT_PORT, bridge: bool = False, debug: bool = False) -> None:
